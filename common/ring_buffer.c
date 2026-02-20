2e103b7f2b6e5d1ffa079978d2636db14a519103
minhcore <dpnhatminh11@gmail.com>
2026-02-20 13:22:50 +0700

    add ring buffer to common/ (ready for uart tx)

--- common/ring_buffer.c
+++ common/ring_buffer.c
@@ -1,1 +1,29 @@
 #include "ring_buffer.h"
+
+bool buffer_empty(struct ring_buffer *bf)
+{
+    return (bf->head == bf->tail);
+}
+
+bool buffer_full(struct ring_buffer *bf)
+{
+    return ((bf->head + 1) & BUFFER_MASK) == bf->tail;
+}
+
+uint8_t read_buffer(struct ring_buffer *bf)
+{
+    if (!buffer_empty(bf)) {
+        uint8_t tmp = bf->buffer[bf->tail];
+        bf->tail = (bf->tail + 1) & BUFFER_MASK;
+        return tmp;
+    }
+    return 0;
+}
+
+void write_buffer(uint8_t data, struct ring_buffer *bf)
+{
+    if (!buffer_full(bf)) {
+        bf->buffer[bf->head] = data;
+        bf->head = (bf->head + 1) & BUFFER_MASK;
+    }
+}
--- common/ring_buffer.h
+++ common/ring_buffer.h
@@ -1,4 +1,22 @@
 #ifndef RING_BUFFER_H
 #define RING_BUFFER_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
+#define BUFFER_SIZE 32
+#define BUFFER_MASK (BUFFER_SIZE - 1)
+
+struct ring_buffer
+{
+    uint8_t head;
+    uint8_t tail;
+    uint8_t *buffer;
+};
+
+bool buffer_empty(struct ring_buffer *bf);
+bool buffer_full(struct ring_buffer *bf);
+uint8_t read_buffer(struct ring_buffer *bf);
+void write_buffer(uint8_t data, struct ring_buffer *bf);
+
 #endif
